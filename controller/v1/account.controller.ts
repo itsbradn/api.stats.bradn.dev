8c58273a5749947b19bfd38418b071de4bb4ae8d
itsbradn <98288965+itsbradn@users.noreply.github.com>
2022-05-05 15:02:56 -0700

    [👤] Added basic user details

--- controller/v1/account.controller.ts
+++ controller/v1/account.controller.ts
@@ -1,14 +1,34 @@
-import { Request, Response } from "express";
+import { Response, Request, NextFunction } from "express";
 import { RequestType } from "../../constant/request.constant";
 import Controller from "../../decorator/controller.decorator";
 import Route from "../../decorator/route.decorator";
-import rateLimitMiddleware from "../../middleware/rateLimit.middleware";
+import protectedMiddleware from "../../middleware/protected.middleware";
+import ErrorResponse from "../../models/errorResponse.model";
 import { AbstractController } from "./account.abstract";
 
 @Controller('/account', 1)
 export class AccountController extends AbstractController {
-    @Route(RequestType.GET, '/', rateLimitMiddleware)
-    GetDetails(req: Request, res: Response): void {
-        res.send('haha');
+    @Route(RequestType.GET, '/', protectedMiddleware)
+    GetDetails(req: Request, res: Response, next: NextFunction): void {
+        if (!req.user.verified) {
+            res.status(403).json({
+                success: false,
+                data: {
+                    id: req.user.id,
+                    username: req.user.username,
+                    email: req.user.email,
+                    verified: false,
+                }
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: {
+                id: req.user.id,
+                username: req.user.username,
+                email: req.user.email,
+            }
+        })
     }
 }

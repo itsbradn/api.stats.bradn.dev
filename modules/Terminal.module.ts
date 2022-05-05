7e4d92433cff032b74e659138472a49dde74acc4
itsbradn <98288965+itsbradn@users.noreply.github.com>
2022-05-05 12:31:54 -0700

    [🎨] Updated terminal visuals

--- modules/Terminal.module.ts
+++ modules/Terminal.module.ts
@@ -4,19 +4,22 @@ import chalk from 'chalk';
 export default (function(cons){
     return {
         log: function(text: string){
-            cons.log(`${getDate()} : ${chalk.blueBright('LOG ')}> ${text}`);
+            cons.log(`[${getDate()}] ${chalk.blueBright.italic('i ')}${text}`);
         },
         info: function (text: string) {
-            cons.log(`${getDate()} : ${chalk.cyanBright('INFO ')}> ${text}`);
+            cons.log(`[${getDate()}] ${chalk.cyanBright('i ')}${text}`);
         },
         warn: function (text: string) {
-            cons.log(`${getDate()} : ${chalk.yellow('WARN ')}> ${text}`);
+            cons.log(`[${getDate()}] ${chalk.bgYellow.black(' WARN ')} ${text}`);
         },
         error: function (text: string) {
-            cons.log(`${getDate()} : ${chalk.redBright('ERROR ')}> ${text}`);
+            cons.log(`[${getDate()}] ${chalk.bgRed.black(' ERROR ')} ${text}`);
         },
         load: function (text: string) {
-            cons.log(`${getDate()} : ${chalk.yellowBright('LOAD ')}> ${text}`);
+            cons.log(`[${getDate()}] ${chalk.yellowBright('LOAD ')}> ${text}`);
+        },
+        success: function (text: string) {
+            cons.log(`[${getDate()}] ${chalk.greenBright('√ ')}${text}`);
         }
     };
 }(console));

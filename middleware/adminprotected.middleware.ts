1b27dab8a1ce59197276d8a652ddbb6b9d82ec3a
itsbradn <98288965+itsbradn@users.noreply.github.com>
2022-05-21 23:54:53 -0700

    [🛡️] Added admin only middleware

---
+++ middleware/adminprotected.middleware.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { decoded } from "../constant/jwtToken.constant";
+import ErrorResponse from "../models/errorResponse.model";
+import userModel from "../models/user.model";
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+        return next(new ErrorResponse("Not Authorized", 401));
+    }
+
+    try {
+        const decoded = <decoded> new Object(verify(token, process.env.JWT_SECRET || "nosecret"));
+        const user = await userModel.findById(decoded.id);
+
+        if (!user) {
+            return next(new ErrorResponse("Invalid Token", 404));
+        }
+
+        if (req.user.rank !== 'admin' || req.user.rank !== 'owner') {
+            return next(new ErrorResponse("Not Authorized", 401));
+        }
+
+        req.user = user;
+        next();
+    } catch (e) {
+        return next(new ErrorResponse("Not Authorized", 401));
+    }
+}
